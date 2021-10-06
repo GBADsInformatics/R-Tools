0cfb68036e81f877f055be765ff9f7f3a03b2ce9
Rehan Nagoor <rehannags@yahoo.com>
2021-10-06 17:03:09 -0400

    added script for print table function

---
+++ print_table.R
@@ -0,0 +1,24 @@
+source("private_details.R")
+
+print_table <- function(){
+
+ library(DBI)
+ library(RPostgres)
+
+ dbname <- "publicData_1"
+ host <- "gbadske-database-public-data.cp73fx22weet.ca-central-1.rds.amazonaws.com"
+ port <- 5432
+ user <- getuser()
+ password <- getpass()
+
+ con <- dbConnect(RPostgres::Postgres(), dbname=dbname, host=host, port=port, user=user, password=password)
+ list <- dbListTables(con)
+ print(list)
+ var <- readline(prompt = "Choose a table to print: ")
+ print(var)
+ var <- paste("SELECT * FROM ", var)
+ table <- dbGetQuery(con, var)
+
+ return(table)
+
+}
